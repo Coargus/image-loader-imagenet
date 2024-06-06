d58972bb98e4e73f6c575680f966f99c94da108b
Minkyu Choi (mc76728) <minkyu.choi@utexas.edu>
2024-06-06 18:16:09 -0500

    update misc func.

--- example.py
+++ example.py
@@ -1,12 +1,13 @@
 """Example run script."""
 
 from cog_cv_imagenet import CogImageNetDataloader
+from cog_cv_imagenet.dataset.cog_dataset import CoargusImageNetDataset
 
 if __name__ == "__main__":
     image_dir = "/store/datasets/ILSVRC"
     loader = CogImageNetDataloader(
         imagenet_dir_path=image_dir, mapping_to="coco"
     )
-    dataset = loader.dataset
+    dataset: CoargusImageNetDataset = loader.dataset
 
     data = dataset.get_all_images_by_label("person")
